eab15a30f4dbdc48cbd76d7991aed5d27099ef58
Peter M. Goldstein <peter.m.goldstein@gmail.com>
2016-04-22 08:50:59 -0700

    Fix configuration such that Dalli is used as a session store.  Switch from pg to sqlite for the demo app.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,4 @@
 # Be sure to restart your server when you modify this file.
 # Rails.application.config.session_store :cookie_store, key: '_dalli_test_app_session'
 
-require 'action_dispatch/middleware/session/dalli_store'
-Rails.application.config.session_store :dalli_store, key: '_ahml_dashboard_session'
+Rails.application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 20.minutes
